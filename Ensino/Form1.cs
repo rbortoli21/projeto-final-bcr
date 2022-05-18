0f27c6d37968d6799023039c75e1c7daa97a0a7f
Rafael bortoli <rafaelbortoli21@gmail.com>
2022-05-18 17:04:27 -0300

    CRUD funcionando e com tratamento de excessões

--- Ensino/Form1.cs
+++ Ensino/Form1.cs
@@ -35,12 +35,14 @@ namespace Ensino
         private void Form1_Load(object sender, EventArgs e)
         {
             dgvCursos.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dgvCursos.DataSource = _cursoRepository.Obter();
+            AtualizarGrid();
         }
 
         private void dgvCategorias_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (dgvCursos.CurrentRow.Index != -1)
+                btnDeletarCurso.Enabled = true;
+            btnDeletarCurso.Enabled = false;
         }
 
         private void btnNovoCurso_Click(object sender, EventArgs e)
@@ -54,22 +56,74 @@ namespace Ensino
                     {
                         _cursoRepository.Cadastrar(curso);
                     }
+                    catch (DuplicateWaitObjectException)
+                    {
+                        MessageBox.Show($"O curso \"{curso.Nome}\" já está cadastrado neste Turno, para poder cadastrá-lo novamente, altere o turno.");
+                        return;
+                    }
                     catch (Exception)
                     {
                         MessageBox.Show($"O curso \"{curso.Nome}\" não pôde ser cadastrado, verifique os campos e tente novamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
                     }
-                    MessageBox.Show($"O curso \"{curso.Nome}\" foi cadastrado com sucesso.", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    
+                    MessageBox.Show($"O curso \"{curso.Nome}\" foi cadastrado com sucesso.", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);      
                 }     
             }
             AtualizarGrid();
         }
        
-
         private void btnEditarCurso_Click(object sender, EventArgs e)
         {
-            var curso = cursoBindingSource.Current as Curso;
-            MessageBox.Show(curso.Nome);
+            var id = Convert.ToInt32(dgvCursos.CurrentRow.Cells[0].Value);
+            var curso = _cursoRepository.ObterPorId(id);
+            using (var form = new FormCurso(curso))
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var curso_n = new Curso
+                        {
+                            Nome = form.textBoxNomeCurso.Text,
+                            Turno = form.comboBoxCursoTurno.Text,
+                            CargaHoraria = (int)form.numericCargaHoraria.Value
+                        };
+                        _cursoRepository.Alterar(curso, curso_n); 
+                    }
+                    catch (DuplicateWaitObjectException)
+                    {
+                        MessageBox.Show($"O curso \"{curso.Nome}\" já está cadastrado neste Turno, para poder cadastrá-lo novamente, altere o turno.");
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"O curso \"{curso.Nome}\" não pôde ser alterado, verifique os dados e tente novamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    MessageBox.Show($"O curso \"{curso.Nome}\" foi alterado com sucesso.", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+            AtualizarGrid();
         }
+
+        private void btnDeletarCurso_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(dgvCursos.CurrentRow.Cells[0].Value);
+            var curso = _cursoRepository.ObterPorId(id);
+            try
+            {
+                _cursoRepository.Deletar(curso);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show($"O curso \"{curso.Nome}\" não pôde ser deletado, tente novamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            MessageBox.Show($"O curso \"{curso.Nome}\" foi deletado com sucesso.", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            AtualizarGrid();
+        }
+        
     }
 }
